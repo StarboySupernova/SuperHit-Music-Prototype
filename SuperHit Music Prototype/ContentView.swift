0b46c483c409698ee75ba0a67bfb3e55a5130099
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2022-02-22 22:21:00 +0000

    Initial Commit

---
+++ SuperHit Music Prototype/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SuperHit Music Prototype
+//
+//  Created by user213454 on 2/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SuperHit Music Prototype/SuperHit_Music_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHit_Music_PrototypeApp.swift
+//  SuperHit Music Prototype
+//
+//  Created by user213454 on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct SuperHit_Music_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
